748a9fa21dc78b8f43218feab325425322872f23
Luciano Bello <luciano.bello@ibm.com>
2019-12-08 08:52:48 -0500

    handle _repo removal

--- scripts/clean.py
+++ scripts/clean.py
@@ -5,7 +5,8 @@ import os.path as op
 path_root = op.join(op.dirname(op.abspath(__file__)), '..')
 
 paths = [op.join(path_root, '_site'),
-         op.join(path_root, '_build')]
+         op.join(path_root, '_build'),
+         op.join(path_root, '_repo')]
 for path in paths:
     print('Removing {}...'.format(path))
     sh.rmtree(path, ignore_errors=True)
