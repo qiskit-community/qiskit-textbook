bf0118ce442168aac8080f933f9cb591598b211c
Frank Harkins <frankharkins@hotmail.co.uk>
2020-02-24 10:15:45 +0000

    Added Widgets, Rewrote section 1.3

---
+++ content/ch-states/widgets.py
@@ -0,0 +1,214 @@
+import ipywidgets as widgets
+from IPython.display import display, clear_output
+from qiskit.visualization import plot_bloch_vector
+from numpy import sqrt, cos, sin, pi
+
+def binary_widget(nbits=5):
+    clear_output()
+    nbits = max(min(10, nbits), 2) # Keep nbits between 2 and 8
+    
+    output = widgets.Output()
+    bits = ['0' for i in range(nbits)]
+    button_list = [widgets.ToggleButton(description=str(2**i),
+                                        layout=widgets.Layout(width='3em',height='3em'))
+                   for i in reversed(range(nbits))]
+
+    hbox = widgets.HBox(button_list)
+    label1 = widgets.Label(value="Toggle the bits below to change the binary number.")
+    label2 = widgets.Label(value="Think of a number between 0 and %i and try to write it down in binary." % ((2**nbits)-1))
+    vbox = widgets.VBox([label1,label2,hbox])
+    def on_btn_click(b):
+        with output:
+            for b in range(nbits):
+                if button_list[b].value:
+                    bits[b] = '1'
+                else:
+                    bits[b] = '0'
+            clear_output(wait=True)
+            string = "".join(bits)
+            print("Binary" + " "*(nbits//2) + " Decimal\n " + string + " = " + str(int(string,2)))
+
+    for button in button_list: button.observe(on_btn_click)
+        
+    # Add output before button press
+    with output:
+        string = "".join(bits)
+        print("Binary" + " "*(nbits//2) + " Decimal\n " + string + " = " + str(int(string,2)))
+    
+    display(vbox, output)
+
+
+def state_vector_exercise(target):
+    clear_output()
+    output = widgets.Output()
+    button = widgets.Button(description="Check", layout=widgets.Layout(width='5em'))
+    text_input = widgets.Text(value='[1, 0]', 
+                              placeholder='Type something', 
+                              width='50px',
+                              disabled=False)
+    
+    label = widgets.Label(value="State Vector:")
+    
+    def on_button_click(b):
+        try:
+            state_vector = eval(text_input.value)
+            c1, c2 = state_vector[0], state_vector[1]
+        except Exception as e:
+            with output:
+                clear_output(wait=True)
+                print(str(e).split("(")[0])
+            return
+        
+        squared_magnitude = abs(c1)**2 + abs(c2)**2
+        p = abs(c1)**2
+        if not (squared_magnitude < 1.01 and squared_magnitude > .99): # Close Enough
+            with output:
+                clear_output(wait=True)
+                print("Magnitude is not equal to 1")
+                return
+        elif p > target*0.99 and p < target*1.01:
+            with output:
+                clear_output(wait=True)
+                print("Correct!")
+        else:
+            with output:
+                clear_output(wait=True)
+                print("The absolute value of " + str(c1) + ", squared is not equal to " + str(target))
+    
+    hbox = widgets.HBox([text_input, button])
+    vbox = widgets.VBox([label, hbox])
+    button.on_click(on_button_click)
+    
+    display(vbox, output)
+
+
+def bloch_calc():
+    clear_output()
+    output = widgets.Output()
+    button = widgets.Button(description="Plot", layout=widgets.Layout(width='4em'))
+    theta_input = widgets.Text(label='$\\theta$',
+                               placeholder='Theta', 
+                               disabled=False)
+    phi_input = widgets.Text(label='$\phi$',
+                             placeholder='Phi', 
+                             disabled=False)
+    
+    label = widgets.Label(value="Define a qubit state using $\\theta$ and $\phi$:")
+    
+    def on_button_click(b):
+        with output:
+            from math import pi, sqrt
+            try:
+                theta = eval(theta_input.value)
+                phi = eval(phi_input.value)
+            except Exception as e:
+                    clear_output()
+                    print("Error: " + str(e))
+                    return
+            x = sin(theta)*cos(phi)
+            y = sin(theta)*sin(phi)
+            z = cos(theta)
+            # Remove horrible almost-zero results
+            if abs(x) < 0.0001:
+                x = 0
+            if abs(y) < 0.0001:
+                y = 0
+            if abs(z) < 0.0001:
+                z = 0
+            clear_output()
+            print("x = r * sin(" + theta_input.value + ") * cos(" + phi_input.value + ")")
+            print("y = r * sin(" + theta_input.value + ") * sin(" + phi_input.value + ")")
+            print("z = r * cos(" + theta_input.value + ")\n")
+            print("Cartesian Bloch Vector = [" + str(x) + ", " + str(y) + ", " + str(z) + "]")
+            display(plot_bloch_vector([x,y,z]))
+    
+    hbox = widgets.HBox([phi_input, button])
+    vbox = widgets.VBox([label, theta_input, hbox])
+    button.on_click(on_button_click)
+    display(vbox, output)
+
+
+def plot_bloch_vector_spherical(coords):
+    clear_output()
+    theta, phi, r = coords[0], coords[1], coords[2]
+    x = r*sin(theta)*cos(phi)
+    y = r*sin(theta)*sin(phi)
+    z = r*cos(theta)
+    output = widgets.Output()
+    return plot_bloch_vector([x,y,z])
+
+
+def gate_demo(gates='full'):
+    clear_output()
+    from qiskit import QuantumCircuit, execute, Aer
+    from qiskit.visualization import plot_bloch_multivector
+    gate_list = []
+    showing_rz = False
+    if 'pauli' in gates:
+        gate_list += ['X','Y','Z']
+    if '+h' in gates:
+        gate_list.append('H')
+    if '+rz' in gates:
+        showing_rz = True
+    if gate_list == [] or gates == 'full':
+        gate_list = ['I','X','Y','Z','H','S','Sdg','T','Tdg']
+        showing_rz = True
+    
+    output = widgets.Output()
+    backend = Aer.get_backend('statevector_simulator')
+    qc = QuantumCircuit(1)
+    button_list = [widgets.Button(description=gate, layout=widgets.Layout(width='3em', height='3em')) for gate in gate_list]
+    button_list.append(widgets.Button(description='Reset', layout=widgets.Layout(width='6em', height='3em')))
+    def update_output():
+        out_state = execute(qc,backend).result().get_statevector()
+        with output:
+            clear_output(wait=True)
+            display(plot_bloch_multivector(out_state))
+    
+    def apply_gates(b,qc):
+        functionmap = {
+            'X':qc.x,
+            'Y':qc.y,
+            'Z':qc.z,
+            'H':qc.h,
+            'S':qc.s,
+            'T':qc.t,
+            'Sdg':qc.sdg,
+            'Tdg':qc.tdg,
+        }
+        if b.description == 'I':
+            pass
+        elif b.description == 'Reset':
+            qc.data = []
+        elif b.description == 'Rz':
+                qc.rz(zrot_slider.value,0)
+        else:
+            functionmap[b.description](0)
+    
+    def on_button_click(b):
+        apply_gates(b,qc)
+        update_output()
+    
+    for button in button_list:
+        button.on_click(on_button_click)
+    if showing_rz:
+        rz_button = widgets.Button(description='Rz', layout=widgets.Layout(width='3em', height='3em'))
+        rz_button.on_click(on_button_click)
+        zrot_slider = widgets.FloatSlider(value=pi, 
+                                         min= -pi,
+                                         max= pi,
+                                         disabled=False,
+                                         readout_format='.2f')
+    qc = QuantumCircuit(1)
+    update_output()
+    
+    if showing_rz:
+        top_box = widgets.HBox(button_list)
+        bottom_box = widgets.HBox([rz_button, zrot_slider])
+        main_box = widgets.VBox([top_box, bottom_box])
+    else:
+        main_box = widgets.HBox(button_list)
+    
+    display(main_box,output)  
+
+
