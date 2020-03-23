d469327f20c96ab3de222b527e76068aaa606358
Frank Harkins <36071638+frankharkins@users.noreply.github.com>
2020-03-23 10:02:26 +0000

    Forgot Commas

--- qiskit-textbook-src/setup.py
+++ qiskit-textbook-src/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
   name='qiskit-textbook',
   version='0.1.0',
-  author='Qiskit Team'
-  author_email='hello@qiskit.org'
+  author='Qiskit Team',
+  author_email='hello@qiskit.org',
   description='''A collection of widgets, tools and games for using along
   the Qiskit Textbook. See the textbook and a list of contributors at qiskit.org/textbook''',
   packages=find_packages(),
