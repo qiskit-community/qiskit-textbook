8ce4d328284864a621f1492a2cfc7e57128953d2
Frank Harkins <frankharkins@hotmail.co.uk>
2020-04-27 16:20:59 +0100

    Revert "png -> jpg (attempt to decrease lag)"
    This reverts commit 758186250a90cdb9c8a21a4da931cbe5ea75a3ec.
    
    had no effect other than to reduce image quality

--- qiskit-textbook-src/qiskit_textbook/widgets/__init__.py
+++ qiskit-textbook-src/qiskit_textbook/widgets/__init__.py
@@ -5,7 +5,6 @@ import ipywidgets as widgets
 from IPython.display import display, clear_output, Math
 from qiskit.visualization import plot_bloch_vector
 from numpy import sqrt, cos, sin, pi
-import numpy as np
 
 from qiskit_textbook.widgets._helpers import _pre, _img
 
@@ -531,3 +530,5 @@ def dj_widget(size="small", case="balanced", display_ancilla=False, hide_oracle=
     image = _img()
     image.value = qc.draw('mpl')
     display(hbox, html_math, image.widget)
+
+
