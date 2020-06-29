d414dde08cc0c7ba506bc93f05fd9445469b40cc
Frank Harkins <frankharkins@hotmail.co.uk>
2020-06-29 16:44:18 +0100

    check line above equation has output_latex tag

--- scripts/process_build.py
+++ scripts/process_build.py
@@ -1,13 +1,14 @@
 # This script santises the sympy latex in the HTML files prior to building the site
 import os
+from html.parser import HTMLParser
 filepath = "./_build/"
 
 def sanitize_latex(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
         newlines = []
-        for line in lines:
-            if line.startswith('$'):
+        for i, line in enumerate(lines):
+            if "output_latex" in lines[i-1] and line.startswith('$'):
                 line = line.replace('{{', '{ {')
             newlines.append(line)
     with open(filename, 'w') as f:
@@ -18,4 +19,3 @@ for directory in os.listdir(filepath):
         for file in os.listdir(filepath + directory):
             if str(file).endswith(".html"):
                 sanitize_latex(filepath + directory + "/" + file)
-
