2f975330f6c36faa3ced3bbfa1dcb3f82b717417
Frank Harkins <frankharkins@hotmail.co.uk>
2020-06-30 09:52:04 +0100

    use os.path

--- scripts/process_build.py
+++ scripts/process_build.py
@@ -1,24 +1,25 @@
 # This script santises the sympy latex in the HTML files prior to building the site
 import os
 from bs4 import BeautifulSoup
-filepath = "./_build/"
+base_dir = "./_build"
 
-def sanitize_latex(filename):
+def sanitize_latex(filepath):
     is_modified = False
-    soup = BeautifulSoup(open(filename), 'html.parser')
+    soup = BeautifulSoup(open(filepath), 'html.parser')
     for latex in soup.find_all('div', {'class':'output_latex'}):
         if "{{" in latex.string:
             sanitized_latex = latex.string.replace("{{", "{ {")
             latex.string = sanitized_latex
             is_modified = True
     if is_modified:
-        print(filename)
-        with open(filename, "w") as f:
+        with open(filepath, "w") as f:
             f.write(str(soup))
 
-for directory in os.listdir(filepath):
-    if os.path.isdir(filepath + directory):
-        for file in os.listdir(filepath + directory):
-            if str(file).endswith(".html"):
-                sanitize_latex(filepath + directory + "/" + file)
+for directory in os.listdir(base_dir):
+    dir_path = os.path.join(base_dir, directory)
+    if os.path.isdir(dir_path):
+        for f in os.listdir(dir_path):
+            if str(f).endswith(".html"):
+                filepath = os.path.join(dir_path, f)
+                sanitize_latex(filepath)
 
