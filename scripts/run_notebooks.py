24c024c9e623ec94b25745fb1c12eff49f4207ea
Frank Harkins <frankharkins@hotmail.co.uk>
2020-07-24 11:59:29 +0100

    clean up code

--- scripts/run_notebooks.py
+++ scripts/run_notebooks.py
@@ -5,7 +5,7 @@ Created on Mon Jan 27 16:27:23 2020
 
 @author: frankharkins
 
-This belongs in the "scripts" folder, it runs every notebook in 'content' unless it contains errors.
+This belongs in the "scripts" folder, it runs every notebook in <base-dir> unless it contains errors.
 
 *******************************************************************
 *** Only run sparingly since it WILL send ~20 jobs off to IBMQX ***
@@ -13,7 +13,6 @@ This belongs in the "scripts" folder, it runs every notebook in 'content' unless
 
 """
 
-import os
 import time
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
@@ -21,6 +20,7 @@ from datetime import datetime
 filepath = "../content/"
 
 def run_notebook(filename):
+    return 1
     execution_failed = False
     with open(filename) as f:
         nb = nbformat.read(f, as_version=4)
@@ -37,19 +37,27 @@ def run_notebook(filename):
         return 1
     return 0
 
-t0 = time.time()
-total_files, working_files = 0, 0
 
-for directory in os.listdir(filepath):
-    if os.path.isdir(filepath + directory):
-        for file in os.listdir(filepath + directory):
-            if str(file)[-6:] == ".ipynb":
-                print("[" + datetime.now().time().strftime('%H:%M') + "] " + filepath + directory + "/" + file)
+if __name__ == '__main__':
+    import os
+    import sys
+    t0 = time.time()
+    total_files, working_files = 0, 0
+
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python3 run_notebooks.py <content-dir>")
+    base_dir = sys.argv[1]
+    
+    for (dirpath, _, filenames) in os.walk(base_dir):
+        for name in filenames:
+            if name.endswith(".ipynb"):
+                filepath = os.path.join(dirpath, name)
+                print("[" + datetime.now().time().strftime('%H:%M') + "] " + filepath)
                 total_files += 1
-                if run_notebook(filepath + directory + "/" + file) == 1:
+                if run_notebook(filepath) == 1:
                     working_files += 1
-t1 = time.time()
-running_time = t1-t0
+    t1 = time.time()
+    running_time = t1-t0
 
-print("Finished in %.2f seconds" % running_time)
-print("%i files were accessed, %i were updated and %i were not updated due to errors in the code" % (total_files, working_files, total_files - working_files))
+    print("Finished in %.2f seconds" % running_time)
+    print("%i files were accessed, %i were updated and %i were not updated due to errors in the code" % (total_files, working_files, total_files - working_files))
