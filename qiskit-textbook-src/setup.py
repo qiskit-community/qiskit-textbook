05807578e3a670f398c418d12eaa7819e5b859d5
Matt Riedemann <mriedem@ibm.com>
2021-11-17 12:02:30 -0600

    Avoid newlines in setup.py description
    The newlines don't work with setuptools 59+ which
    causes pip install to fail.
    
    Closes #1327

--- qiskit-textbook-src/setup.py
+++ qiskit-textbook-src/setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup, find_packages
+description = ('A collection of widgets, tools and games for using along '
+               'the Qiskit Textbook. See the textbook and a list of '
+               'contributors at qiskit.org/textbook')
 
 setup(
   name='qiskit-textbook',
   version='0.1.0',
   author='Qiskit Team',
   author_email='hello@qiskit.org',
-  description='''A collection of widgets, tools and games for using along
-  the Qiskit Textbook. See the textbook and a list of contributors at qiskit.org/textbook''',
+  description=description,
   packages=find_packages(),
   install_requires=[
     'qiskit',
